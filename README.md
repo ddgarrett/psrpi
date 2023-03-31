c00765b9f9f03ba350ca20170b787b4e7c9ca178
ddgarrett <doug@anetsc.com>
2023-03-31 06:10:01 -0700

    Add Ideas for New Modules

--- README.md
+++ README.md
@@ -6,5 +6,16 @@ Based on concepts developed on the PSOS project. Like the PSOS project, Publish 
 
 To run:
 1. Install Paho MQTT client  > pip install paho-mqtt
-e. Install asyncio-mqtt      > pip install asyncio-mqtt
+2. Install asyncio-mqtt      > pip install asyncio-mqtt
+
+Thing to use this framework for:
+1. an MQTT smart bridge (elminate duplicates - use lan/ topic prefix)
+    - mod_bridge
+2. web server to monitor/control devices
+3. save and maybe serve up, MQTT messages via filter, count, start position
+    - mod_log_srch
+4. trigger to periodically generate messages (like e01 "svc_pub_timer" does now)
+    - mod_timer
+5. access/update MongoDB - either local or remote
+6. generalized web sockets interface to MQTT?
 
---
+++ mod_timer.py
@@ -0,0 +1,56 @@
+"""
+    Timer Module Class
+    
+    Trigger an even every n seconds
+    
+    pub_msg = message to publish
+    pub_topics = [list] of topics to publish to
+    pub_wait   = number of seconds to wait between publishing
+    
+    Service will wait 10ms between publishing so it doesn't send
+    too many messages in a short period of time.
+    
+"""
+
+from ps_mod import PsrpiModule
+import asyncio
+import time
+
+
+# All initialization classes are named ModuleService
+class ModuleService(PsrpiModule):
+    
+    def __init__(self, parms):
+        super().__init__(parms)
+        
+    async def run(self):
+        
+        mqtt   = self.get_mqtt()
+        
+        # parameters
+        topics = self.get_parm("pub_topics",[])
+        msg    = self.get_parm("pub_msg",[])
+        wait = self.get_parm("pub_wait",5*60)
+        wait_ms   = wait*1000
+        initial_wait_ms = self.get_parm("init_wait",wait)*1000
+        sleep     = self.get_parm("sleep_ms",10)
+        
+        # function aliases 
+        ticks_add = time.ticks_add
+        ticks_ms  = time.ticks_ms
+        sleep_ms  = asyncio.sleep_ms       
+       
+        next_time = ticks_add(ticks_ms(),initial_wait_ms)
+        
+        while True:
+            await sleep_ms(ticks_add(next_time,-ticks_ms()))
+            next_time = ticks_add(next_time,wait_ms)
+            
+            for i in range(len(topics)):
+                if i >= len(msg):
+                    m = ""
+                else:
+                    m = msg[i]
+                    
+                await mqtt.publish(topics[i],m)
+                await sleep_ms(sleep)
